4c7bd657ea18c085f354291bbe29d87df5082faf
Omaroovee <Muhammad.omar.eg@gmail.com>
2018-06-06 08:21:08 +0200

    adding your slackbot tokens

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const SlackBot = require('slackbots');
 const axios = require('axios');
+
+const bot = new SlackBot({
+    token: 'xoxb-Your own key',
+    name: 'Wattary'
+});
+
+bot.on('start', () => {
+    const params = {
+        icon_emoji: ':robot_face:'
+    }
+
+    // define channel, where bot exist. You can adjust it there https://my.slack.com/services 
+    bot.postMessageToChannel('house_of_cards_fans', 'Hello , what can i do for you ?', params);
+})
+
